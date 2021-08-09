6a689a491513c830cf3bbe6a351cd0099e2a7e92
GodChiken <godchiken@naver.com>
2021-08-09 16:09:23 +0900

    버전을 따로 관리하도록 시도, 프로퍼티 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+// 그레들 레퍼런스
 // https://docs.gradle.org/current/dsl/org.gradle.api.Project.html#org.gradle.api.Project:artifacts(groovy.lang.Closure)
-val kotlinVersion: String = "1.5.21"
+
 
 // https://notpeelbean.tistory.com/entry/gradle-buildscript-dependencies-%EC%99%80-dependencies-%EC%9D%98-%EC%B0%A8%EC%9D%B4
+// 빌드 시 repository, depdency를 활용하여 참조하는 요소를 기술하는 곳, plugins{} 블럭에서 버전을 지정하면 그외의 곳에서 동일한 버전을 따라간다.
+// 버전을 설정해야 하는 유일한 장소는 플러그인의 클래스 경로를 정의할 때 뿐이므로 본인은 삭제했다.
 buildscript {
-    ext{
-        val asdf_version = "1.5.21"
-    }
     repositories { mavenCentral() }
-    dependencies { classpath( "org.jetbrains.kotlin:kotlin-noarg:${findProperty("kotlin_version ")}") }
+    dependencies { classpath( "org.jetbrains.kotlin:kotlin-noarg") }
 }
 
 // https://plugins.gradle.org/ 위 사이트에서 플러그인 정보를 확인한다.
+// https://stackoverflow.com/questions/47588311/gradle-kotlin-dsl-define-kotlin-version-in-unique-place 버전을 따로 관리하려고 시도해보면서 얻은 결론.
 /**
  * 프로젝트를 빌드하기 위해서 여러가지 작업을 처리해 줘야 합니다. 컴파일이나 jar 파일의 생성 같은 작업들이죠.
  * 이런 작업들을 해주는 플러그인들이 존재합니다. plugins 블록 안에 필요한 플러그인을 지정해주고 이런 플로그인들은 필요한 과정들을 task로 포함하고 있습니다.
@@ -22,9 +23,9 @@ plugins {
     val kotlinVersion = "1.5.21"
     id("org.springframework.boot") version "2.5.3" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm:${kotlinVersion}")
-    kotlin("plugin.spring:${kotlinVersion}")
-    kotlin("plugin.jpa:${kotlinVersion}")
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
 }
 
 // 전체 프로젝트에 적용할 내용? 인거같은데 확인이 필요
