f9010a632617c54c912d01e0750410fb3fc56cd6
GodChiken <godchiken@naver.com>
2021-09-02 17:28:15 +0900

    GraphQL Scalar Extends 추가

---
+++ api/src/main/kotlin/com/masterkbh/kotlin/api/graphql/scalar/ScalarExtendRegistration.kt
@@ -0,0 +1,17 @@
+package com.masterkbh.kotlin.api.graphql.scalar
+
+import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsRuntimeWiring
+import graphql.scalars.ExtendedScalars
+import graphql.schema.idl.RuntimeWiring
+
+
+@DgsComponent
+class ScalarExtendRegistration {
+    @DgsRuntimeWiring
+    fun addScalar(builder: RuntimeWiring.Builder): RuntimeWiring.Builder {
+        return builder.scalar(ExtendedScalars.Date)
+                      .scalar(ExtendedScalars.DateTime)
+                      .scalar(ExtendedScalars.GraphQLLong)
+    }
+}
