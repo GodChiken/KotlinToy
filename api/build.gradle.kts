1afcbe5bbdbf22715cd3e8c11b15c802f074815f
GodChiken <godchiken@naver.com>
2021-09-06 18:55:03 +0900

    코드 정리

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -9,3 +9,11 @@ dependencies {
     implementation("com.netflix.graphql.dgs:graphql-dgs-extended-scalars")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-joda")
 }
+
+tasks.withType<com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask> {
+    packageName = "com.masterkbh.kotlin.generated"
+    typeMapping = mutableMapOf(
+        "BigDecimal" to "java.math.BigDecimal",
+        "Long" to "kotlin.Long"
+    )
+}
--- api/src/main/kotlin/com/masterkbh/kotlin/api/graphql/component/fetcher/TestEntityFetcher.kt
+++ api/src/main/kotlin/com/masterkbh/kotlin/api/graphql/component/fetcher/TestEntityFetcher.kt
@@ -11,11 +11,65 @@ class TestEntityFetcher(
     val testService: TestService
 ) {
     @DgsQuery
-    fun testList(@InputArgument idFilter: Long?): List<TestEntityDTO> {
-        return if (idFilter != null) {
-            testService.findAll().filter { e -> e.id == idFilter }
-        } else {
-            testService.findAll()
-        }
+    fun testList(
+        @InputArgument idFilter: Long?,
+        @InputArgument secretContain: String?,
+        @InputArgument nameContain: String?,
+        @InputArgument nickNameContain: String?,
+        @InputArgument addressContain: String?
+    ): List<TestEntityDTO> {
+        val testList = testService.findAll()
+        getIDFilteredList(idFilter, testList)
+        getSecretContainContainFilteredList(secretContain, testList)
+        getNameContainFilteredList(nameContain, testList)
+        getNickNameContainContainFilteredList(nickNameContain, testList)
+        getAddressContainFilteredList(addressContain, testList)
+        return testList
     }
+
+    private fun getIDFilteredList(
+        idFilter: Long?,
+        testList: List<TestEntityDTO>
+    ) = if (idFilter != null) {
+        testList.filter { e -> e.id == idFilter }
+    } else {
+        testList
+    }
+
+    private fun getSecretContainContainFilteredList(
+        secretContain: String?,
+        testList: List<TestEntityDTO>
+    ) = if (secretContain != null) {
+        testList.filter { e -> e.secret?.contains(secretContain) ?: throw IllegalArgumentException("값을 적으세요") }
+    } else {
+        testList
+    }
+
+    private fun getNameContainFilteredList(
+        nameContain: String?,
+        testList: List<TestEntityDTO>
+    ) = if (nameContain != null) {
+        testList.filter { e -> e.name?.contains(nameContain) ?: throw IllegalArgumentException("값을 적으세요") }
+    } else {
+        testList
+    }
+
+    private fun getNickNameContainContainFilteredList(
+        nickNameContain: String?,
+        testList: List<TestEntityDTO>
+    ) = if (nickNameContain != null) {
+        testList.filter { e -> e.nickName?.contains(nickNameContain) ?: throw IllegalArgumentException("값을 적으세요") }
+    } else {
+        testList
+    }
+
+    private fun getAddressContainFilteredList(
+        addressContain: String?,
+        testList: List<TestEntityDTO>
+    ) = if (addressContain != null) {
+        testList.filter { e -> e.address?.contains(addressContain) ?: throw IllegalArgumentException("값을 적으세요") }
+    } else {
+        testList
+    }
+
 }
