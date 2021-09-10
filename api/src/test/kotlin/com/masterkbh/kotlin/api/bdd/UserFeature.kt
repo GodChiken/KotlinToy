212deb25671574e6299e8b9ba2b43c83d0edd1a6
GodChiken <godchiken@naver.com>
2021-09-10 17:21:59 +0900

    Kotest Test 세팅중 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)  : 자동으로 인메모리DB를 세팅하는 것을 방지하기 위함 @PropertySource("classpath:application.yml")  : 프로젝트 세팅 프로퍼티 @EnableAutoConfiguration(exclude=[DataSourceAutoConfiguration::class])  : https://stackoverflow.com/questions/28042426/spring-boot-error-creating-bean-with-name-datasource-defined-in-class-path-r [확인중]

--- api/src/test/kotlin/com/masterkbh/kotlin/api/bdd/UserFeature.kt
+++ api/src/test/kotlin/com/masterkbh/kotlin/api/bdd/UserFeature.kt
@@ -4,33 +4,34 @@ import com.masterkbh.kotlin.api.config.SpringDataConfig
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.PropertySource
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.transaction.annotation.Transactional
 
 
-@EnableAutoConfiguration(exclude = [DataSourceAutoConfiguration::class])
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@PropertySource("classpath:application.yml")
+//@EnableAutoConfiguration(exclude=[DataSourceAutoConfiguration::class])
 @ContextConfiguration(classes = [SpringDataConfig::class])
 @ActiveProfiles(profiles = ["dev"])
-class UserFeature @Autowired constructor(
-    //testRepository : TestRepository
-) : BehaviorSpec() {
+@ComponentScan(basePackages = ["com.masterkbh.kotlin.api", "com.masterkbh.kotlin.common"])
+@Transactional
+class UserFeature : BehaviorSpec() {
 
-/*
-    private val mockTestRepository = spyk(testRepository)
-    private val mockTestService = TestService(testRepository)
-*/
+    //private val mockTestRepository = spyk(testRepository)
+    //private val mockTestService = TestService(testRepository)
 
     //https://kotest.io/docs/framework/isolation-mode.html
     override fun isolationMode(): IsolationMode = IsolationMode.InstancePerLeaf
 
     init {
-        given("불특정 요청자가 나의 제공 서비스중"){
-            When("어떤 것을 사용하든"){
-                and("테스트객체 추가 API를 요청하면"){
-                    then("테스트 객체가 추가된다."){
+        given("불특정 요청자가 나의 제공 서비스중") {
+            When("어떤 것을 사용하든") {
+                and("테스트객체 추가 API를 요청하면") {
+                    then("테스트 객체가 추가된다.") {
                         0 shouldBe 0
                     }
                 }
