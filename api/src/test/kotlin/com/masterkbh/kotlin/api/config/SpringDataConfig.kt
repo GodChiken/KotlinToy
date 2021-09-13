475dce6168ca1c03fd08eb2563d92e3fb1e602fb
GodChiken <godchiken@naver.com>
2021-09-13 18:14:57 +0900

    해결책을 찾는중

--- api/src/test/kotlin/com/masterkbh/kotlin/api/config/SpringDataConfig.kt
+++ api/src/test/kotlin/com/masterkbh/kotlin/api/config/SpringDataConfig.kt
@@ -1,17 +1,44 @@
 package com.masterkbh.kotlin.api.config
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.extensions.Extension
 import io.kotest.spring.SpringAutowireConstructorExtension
 import io.kotest.spring.SpringListener
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.PropertySource
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import javax.sql.DataSource
+
 
 @DataJpaTest
+<<<<<<< Updated upstream
 @EntityScan(basePackages = ["com.masterkbh.kotlin.common"])
 @EnableJpaRepositories(basePackages = ["com.masterkbh.kotlin.common"])
+=======
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@PropertySource("classpath:application.yml")
+@EntityScan(basePackages = ["com.masterkbh.kotlin.common.entity"])
+@EnableJpaRepositories(basePackages = ["com.masterkbh.kotlin.common.repository"])
+>>>>>>> Stashed changes
 class SpringDataConfig : AbstractProjectConfig() {
     override fun listeners() = listOf(SpringListener)
     override fun extensions(): List<Extension> = listOf(SpringAutowireConstructorExtension)
+<<<<<<< Updated upstream
+=======
+
+    @Primary
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    fun hikariConfig(): HikariConfig? {
+        return HikariConfig()
+    }
+
+>>>>>>> Stashed changes
 }
